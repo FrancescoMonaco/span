81c809e707b1430f5de691d9d511b728498972f7
francescopio.monaco@studenti.unipd.it <francescopio.monaco@studenti.unipd.it>
2025-02-04 14:31:38 +0100

    Init

---
+++ build/CMakeFiles/GloveExample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GloveExample.dir/examples/glove.cpp.o"
+  "CMakeFiles/GloveExample.dir/examples/glove.cpp.o.d"
+  "GloveExample"
+  "GloveExample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GloveExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
