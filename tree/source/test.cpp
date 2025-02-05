5b4dcbc8ffba34f32e4bdaa6f94b546f7300b050
francescopio.monaco@studenti.unipd.it <francescopio.monaco@studenti.unipd.it>
2025-02-05 10:26:20 +0100

    Adding discretized random projections

--- tree/source/test.cpp
+++ tree/source/test.cpp
@@ -1,7 +1,26 @@
 #include <omp.h>
+#include <puffinn.hpp>
+#include <catch.hpp>
 #include <stdio.h>
+#include <random>
+#include <chrono>
+#include <thread>
+
 int main() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<std::mt19937::result_type> dist(1, 10);
+    auto num = dist(gen);
+
 #pragma omp parallel
-    printf("Hello from thread %d\n", omp_get_thread_num());
+    #pragma omp critical
+    {
+        printf("Hello from thread %d\n", omp_get_thread_num());
+    }
+
+    #pragma omp master
+        printf("Hello from master thread\n");
+
     return 0;
+
 }
